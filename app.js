c9490b36c36fd683cc988030cdff0ce94bc5bc49
Matías Salicrú <mattiassalicru@gmail.com>
2021-06-22 19:39:18 -0300

    Mostrar menu y pausa implementado

---
+++ app.js
@@ -0,0 +1,13 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear()
+
+const main = async () => {
+    console.log('Hola mundo!');
+    mostrarMenu();
+    pausa();
+}
+
+main();
